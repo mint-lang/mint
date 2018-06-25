b0fcec02bda75021cd8be3862753599d7db00434
Gusztav Szikszai <guszti5@hotmail.com>
2018-06-25 16:50:07 +0200

    Add description field to computed properties.

--- src/documentation_server/get.cr
+++ src/documentation_server/get.cr
@@ -3,6 +3,7 @@ module Mint
     def generate(node : Ast::Get, json : JSON::Builder)
       json.object do
         json.field "name", node.name.value
+        json.field "description", node.comment.try(&.to_html)
         json.field "type", stringify(node.type)
         json.field "source", source(node)
       end
