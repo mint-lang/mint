ce341f8cb904c8f44ddfdbec2fa1babdd2fede5a
Gusztav Szikszai <guszti5@hotmail.com>
2018-08-08 09:44:33 +0200

    Fix typo in access not a record message.

--- src/messages/access_not_record.cr
+++ src/messages/access_not_record.cr
@@ -2,7 +2,7 @@ message AccessNotRecord do
   title "Type Error"
 
   block do
-    text "You are tyring to access a field on an object which is not a record:"
+    text "You are trying to access a field on an object which is not a record:"
   end
 
   type object
