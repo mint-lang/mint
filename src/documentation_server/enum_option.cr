cea0c1a2bca0676026e4d0ce617e385bb30b5034
Gusztav Szikszai <guszti5@hotmail.com>
2018-09-09 09:11:12 +0200

    Update documentation server with enum parameters.

--- src/documentation_server/enum_option.cr
+++ src/documentation_server/enum_option.cr
@@ -4,6 +4,10 @@ module Mint
       json.object do
         json.field "description", node.comment.try(&.to_html)
         json.field "name", node.value
+
+        json.field "parameters" do
+          generate node.parameters, json
+        end
       end
     end
   end
--- src/documentation_server/type_variable.cr
+++ src/documentation_server/type_variable.cr
@@ -3,5 +3,9 @@ module Mint
     def stringify(node : Ast::TypeVariable)
       node.value
     end
+
+    def generate(node : Ast::TypeVariable, json : JSON::Builder)
+      json.string node.value
+    end
   end
 end
