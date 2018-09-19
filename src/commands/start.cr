ff94fc58a734eabebec44888659fbf66ee19f982
Gusztav Szikszai <guszti5@hotmail.com>
2018-09-19 09:05:20 +0200

    Rename no_service_worker to skip_service_worker && cleanup command flags.

--- src/commands/start.cr
+++ src/commands/start.cr
@@ -4,22 +4,23 @@ module Mint
       include Command
 
       define_help description: "Starts the development server"
+
       define_flag auto_format : Bool,
         description: "Auto formats the source files when running development server",
-        default: false,
-        required: false
+        required: false,
+        default: false
+
       define_flag host : String,
         description: "Change the host to serve the application on. (Default: 127.0.0.1)",
-        long: host,
-        short: h,
         default: ENV["HOST"]? || "127.0.0.1",
-        required: false
+        required: false,
+        short: "h"
+
       define_flag port : Int32,
         description: "Change the port to serve the application on. (Default: 3000)",
-        long: port,
-        short: p,
         default: (ENV["PORT"]? || "3000").to_i,
-        required: false
+        required: false,
+        short: "p"
 
       def run
         execute "Running the development server" do
