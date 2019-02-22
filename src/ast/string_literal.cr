6683557bae74b6780d974391ff487147ac21daa2
Gusztav Szikszai <guszti5@hotmail.com>
2019-02-22 09:06:58 +0100

    Don't automatically split long strings only if they are split at least once.

--- src/ast/string_literal.cr
+++ src/ast/string_literal.cr
@@ -1,9 +1,10 @@
 module Mint
   class Ast
     class StringLiteral < Node
-      getter value
+      getter value, broken
 
       def initialize(@value : String,
+                     @broken : Bool,
                      @input : Data,
                      @from : Int32,
                      @to : Int32)
