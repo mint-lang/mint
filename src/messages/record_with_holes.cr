c08ee08bf6f581e2845e89f711fe28913d43439d
Gusztav Szikszai <guszti5@hotmail.com>
2019-06-05 17:22:40 +0200

    Added missing file.

---
+++ src/messages/record_with_holes.cr
@@ -0,0 +1,9 @@
+message RecordWithHoles do
+  title "Type Error"
+
+  block do
+    text "Records with type variables are not allow at this time."
+  end
+
+  snippet node, "The record in question is defined here:"
+end
