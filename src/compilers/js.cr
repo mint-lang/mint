bf9be847bd65580742b596705f585d5da4d79fd1
Gusztav Szikszai <guszti5@hotmail.com>
2019-09-05 13:43:20 +0200

    Parenthesize JS interpolated values to avoid confusion.
    Resolves #135

--- src/compilers/js.cr
+++ src/compilers/js.cr
@@ -1,14 +1,21 @@
 module Mint
   class Compiler
     def _compile(node : Ast::Js) : String
-      node.value.map do |item|
-        case item
-        when Ast::Node
-          compile item
-        else
-          item
-        end
-      end.join("").strip
+      value =
+        node.value.map do |item|
+          case item
+          when Ast::Node
+            compile item
+          else
+            item
+          end
+        end.join("").strip
+
+      if value.empty?
+        ""
+      else
+        "(#{value})"
+      end
     end
   end
 end
