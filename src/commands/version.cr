05a32517ba225bd50b325000126d4892c5afa5cd
Kevin Sjöberg <kevin@striver.se>
2020-02-18 16:50:06 +0100

    Refactor mint version command

--- src/commands/version.cr
+++ src/commands/version.cr
@@ -7,16 +7,9 @@ module Mint
 
       define_help description: "Shows version"
 
-      macro read_file_at_compile_time(filename)
-        # Will work until Crystal supports Windows
-        {{ `cat #{filename}`.stringify }}
-      end
-
       def run
         execute "Showing version" do
-          shard_file = read_file_at_compile_time("shard.yml")
-          version = YAML.parse(shard_file)["version"]
-          puts "Mint #{version}"
+          puts "Mint #{Mint::VERSION}"
         end
       end
     end
