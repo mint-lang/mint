0c6a3932b7a8d7fcdd32937d2da9f02ec4427b67
Gusztáv Szikszai <guszti5@hotmail.com>
2020-09-27 09:11:06 +0200

    Format the type signature of an array literal if given.

--- src/formatters/array_literal.cr
+++ src/formatters/array_literal.cr
@@ -7,13 +7,21 @@ module Mint
       items =
         format node.items, multiline ? ",\n" : ", "
 
-      if node.items.empty?
-        "[]"
-      elsif multiline || replace_skipped(items).includes?('\n')
-        "[\n#{indent(items)}\n]"
-      else
-        "[#{items}]"
-      end
+      type =
+        node.type.try do |item|
+          " of #{format(item)}"
+        end
+
+      head =
+        if node.items.empty?
+          "[]"
+        elsif multiline || replace_skipped(items).includes?('\n')
+          "[\n#{indent(items)}\n]"
+        else
+          "[#{items}]"
+        end
+
+      "#{head}#{type}"
     end
   end
 end
