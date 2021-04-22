2e0ede7f3f40867c77cba9b0ea8035b48ce91000
Gusztáv Szikszai <guszti5@hotmail.com>
2021-04-22 06:38:59 +0200

    Added CORS handler.

--- src/sandbox_server.cr
+++ src/sandbox_server.cr
@@ -1,5 +1,26 @@
 module Mint
   class SandboxServer
+    class Cors
+      include HTTP::Handler
+
+      def call(context)
+        context.response.headers["Access-Control-Allow-Methods"] = "GET, PUT, POST, DELETE, LOAD, PATCH"
+        context.response.headers["Access-Control-Request-Headers"] = "Content-Type, *"
+        context.response.headers["Access-Control-Allow-Headers"] = "authorization"
+        context.response.headers["Access-Control-Allow-Origin"] = "*"
+        context.response.headers["Access-Control-Allow-Credentials"] = "true"
+        context.response.headers["Access-Control-Max-Age"] = "1728000"
+
+        if context.request.method.downcase == "options"
+          context.response.content_type = "text/html; charset=utf-8"
+          context.response.status_code = 200
+          context.response.print("")
+        else
+          call_next context
+        end
+      end
+    end
+
     struct File
       include JSON::Serializable
 
@@ -17,7 +38,7 @@ module Mint
     end
 
     def initialize(@host = "0.0.0.0", @port = 8080)
-      @server = HTTP::Server.new { |context| handle_request(context) }
+      @server = HTTP::Server.new([Cors.new]) { |context| handle_request(context) }
       @formatter = Formatter.new
       @core = Core.ast
     end
