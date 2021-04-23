0d1bdbd2c2851ce3e385ab76fcada2a1e52a4d41
Sijawusz Pur Rahnama <sija@sija.pl>
2021-04-23 11:33:12 +0200

    Move the middleware class closer to the place of its usage

--- src/sandbox_server.cr
+++ src/sandbox_server.cr
@@ -1,24 +1,5 @@
 module Mint
   class SandboxServer
-    class CORS
-      include HTTP::Handler
-
-      def call(context)
-        context.response.headers["Access-Control-Allow-Origin"] = "*"
-        context.response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, PATCH"
-        context.response.headers["Access-Control-Allow-Credentials"] = "true"
-        context.response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type"
-        context.response.headers["Access-Control-Max-Age"] = 1.day.total_seconds.to_i.to_s
-
-        if context.request.method.upcase == "OPTIONS"
-          context.response.content_type = "text/html; charset=utf-8"
-          context.response.status = :ok
-        else
-          call_next context
-        end
-      end
-    end
-
     struct File
       include JSON::Serializable
 
@@ -35,6 +16,25 @@ module Mint
       getter files : Array(File)
 
       def initialize(@files)
+      end
+    end
+
+    class CORS
+      include HTTP::Handler
+
+      def call(context)
+        context.response.headers["Access-Control-Allow-Origin"] = "*"
+        context.response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, PATCH"
+        context.response.headers["Access-Control-Allow-Credentials"] = "true"
+        context.response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type"
+        context.response.headers["Access-Control-Max-Age"] = 1.day.total_seconds.to_i.to_s
+
+        if context.request.method.upcase == "OPTIONS"
+          context.response.content_type = "text/html; charset=utf-8"
+          context.response.status = :ok
+        else
+          call_next context
+        end
       end
     end
 
