59179df5a04642bf39c94a00a5a80126a434599e
Sijawusz Pur Rahnama <sija@sija.pl>
2021-05-02 16:32:27 +0200

    Update spec/compilers_spec.cr

--- spec/compilers_spec.cr
+++ spec/compilers_spec.cr
@@ -15,10 +15,10 @@ Dir
 
       # Compare results
       result = Mint::Compiler.compile_bare(Mint::TypeChecker.check(ast), {
+        css_prefix: nil,
         optimize:   false,
         relative:   false,
         build:      true,
-        css_prefix: nil,
       })
 
       begin
