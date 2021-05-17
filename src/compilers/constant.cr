ef1d8c2eceac5f34a6074c2d38e1f41bf8ccb82e
Gusztáv Szikszai <guszti5@hotmail.com>
2021-05-17 12:34:27 +0200

    Make sure constants are compiled in resolve order.

--- src/compilers/constant.cr
+++ src/compilers/constant.cr
@@ -3,6 +3,7 @@ module Mint
     def compile_constants(nodes : Array(Ast::Constant)) : Hash(String, String)
       nodes
         .select(&.in?(checked))
+        .sort_by! { |item| resolve_order.index(item) || -1 }
         .each_with_object({} of String => String) do |node, memo|
           memo[js.variable_of(node)] =
             js.arrow_function(%w[], js.return(compile(node.value)))
