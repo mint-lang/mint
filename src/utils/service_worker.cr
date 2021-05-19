85db389a69ac6df35639946709b806f9537ddfc9
Sijawusz Pur Rahnama <sija@sija.pl>
2021-05-19 22:10:16 +0200

    Extract the JS source out of `ServiceWorker`

--- src/utils/service_worker.cr
+++ src/utils/service_worker.cr
@@ -1,82 +1,10 @@
 module Mint
   class ServiceWorker
-    SOURCE =
-      <<-JS
-      // On install precache all static resources
-      self.addEventListener('install', event => {
-        event.waitUntil(
-          caches
-            .open(CACHE)
-            .then(cache =>  {
-              const promises =
-                PRECACHE_URLS.map((url) =>
-                  cache
-                    .add(url)
-                    .catch(error => console.log(`Could not cache: ${url} - ${error}!`))
-                )
-
-              return Promise.all(promises)
-            })
-            .then(self.skipWaiting())
-        );
-      });
-
-      // On activate remove all unused caches
-      self.addEventListener('activate', function(event) {
-        event.waitUntil(
-          caches.keys().then(cacheNames => {
-            return cacheNames.filter(cacheName => cacheName !== CACHE);
-          }).then(cachesToDelete => {
-            return Promise.all(cachesToDelete.map(cacheToDelete => {
-              return caches.delete(cacheToDelete);
-            }));
-          }).then(() => self.clients.claim())
-        );
-      });
-
-      self.addEventListener('fetch', event => {
-        const url = event.request.url
-        const origin = self.location.origin
-        const isSameOrigin = url.startsWith(origin)
-        let response = null
-
-        // If we are on the same origin
-        if (isSameOrigin) {
-          // resolve the path
-          const path = url.slice(origin.length)
-
-          // Try to get the response from the cache if not available fall back to
-          // the "index.html" file.
-          response =
-            caches
-              .match(event.request)
-              .then(cachedResponse => cachedResponse || caches.match("/index.html"))
-        } else {
-          response = fetch(event.request)
-        }
-
-        event.respondWith(response)
-      });
-      JS
-
     def self.generate
-      new.generate
+      new.to_s
     end
 
-    def generate
-      Dir.cd DIST_DIR
-
-      head =
-        <<-JS
-        const CACHE = '#{calculate_hash}';
-        const RUNTIME = 'runtime';
-        const PRECACHE_URLS = [#{files}];
-        JS
-
-      Dir.cd ".."
-
-      {head, SOURCE}.join("\n\n")
-    end
+    ECR.def_to_s "#{__DIR__}/service_worker.ecr"
 
     def files
       Dir
