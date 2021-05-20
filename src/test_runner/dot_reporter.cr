0154f16fb873210c15eb3c61c9a5b5ad7e3fc286
Sijawusz Pur Rahnama <sija@sija.pl>
2021-05-20 18:25:51 +0200

    Optimize `TestRunner::DotReporter` a bit

--- src/test_runner/dot_reporter.cr
+++ src/test_runner/dot_reporter.cr
@@ -1,25 +1,30 @@
 module Mint
   class TestRunner
     class DotReporter
+      private MAX_LINE_LENGTH = 80
+
+      private SUCCESS_MARKER = ".".colorize(:green)
+      private FAIL_MARKER    = "F".colorize(:red)
+
       def initialize
         @count = 0
       end
 
       def with_count
-        puts if @count > 0 && @count % 80 == 0
+        puts if @count > 0 && @count % MAX_LINE_LENGTH == 0
         yield
         @count += 1
       end
 
       def succeeded(name)
         with_count do
-          print ".".colorize(:green)
+          print SUCCESS_MARKER
         end
       end
 
       def failed(name, error)
         with_count do
-          print "F".colorize(:red)
+          print FAIL_MARKER
         end
       end
 
