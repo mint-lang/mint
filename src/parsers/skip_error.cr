420d7958394d69e297ce28c7c2bd147142a51579
Gusztáv Szikszai <guszti5@hotmail.com>
2021-05-23 10:09:26 +0200

    Don't compute callstack for `SkipError`
    This is a monkey patch for raising `SkipError` exceptions without
    computing the callstack. It results in quicker parsing.

--- src/parsers/skip_error.cr
+++ src/parsers/skip_error.cr
@@ -2,5 +2,15 @@ module Mint
   class Parser
     class SkipError < Exception
     end
+
+    # This is similar to `raise(Exception)` except that it doesn't compute a callstack.
+    def raise(exception : Mint::Parser::SkipError) : NoReturn
+      unwind_ex = Pointer(LibUnwind::Exception).malloc
+      unwind_ex.value.exception_class = LibC::SizeT.zero
+      unwind_ex.value.exception_cleanup = LibC::SizeT.zero
+      unwind_ex.value.exception_object = exception.as(Void*)
+      unwind_ex.value.exception_type_id = exception.crystal_type_id
+      __crystal_raise(unwind_ex)
+    end
   end
 end
