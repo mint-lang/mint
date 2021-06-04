0e42ca0b63916d4150de56c9d88c6b03ed72997d
Sijawusz Pur Rahnama <sija@sija.pl>
2021-06-05 01:53:01 +0200

    Further refactoring of svg compiler directive

--- src/compilers/directives/svg.cr
+++ src/compilers/directives/svg.cr
@@ -5,21 +5,27 @@ module Mint
         static_components_pool.of(node.path, nil)
 
       static_components[name] ||= begin
-        parsed =
-          parse_svg(node.file_contents)
-
-        return "" unless parsed
-
-        width, height, view_box, data =
-          parsed
-
-        attributes =
-          "{ width: '#{width}', height: '#{height}', viewBox: '#{view_box}', dangerouslySetInnerHTML: { __html: `#{data}` }}"
-
-        "_h('svg', #{attributes})"
+        value = compile_svg_directive(node)
+        return "" unless value
+        value
       end
 
       "$#{name}()"
+    end
+
+    def compile_svg_directive(node : Ast::Directives::Svg) : String?
+      parsed =
+        parse_svg(node.file_contents)
+
+      return unless parsed
+
+      width, height, view_box, data =
+        parsed
+
+      attributes =
+        "{ width: '#{width}', height: '#{height}', viewBox: '#{view_box}', dangerouslySetInnerHTML: { __html: `#{data}` }}"
+
+      "_h('svg', #{attributes})"
     end
 
     def parse_svg(contents)
