658c61812593b8a1952cd46988027cd600e35d5e
Sijawusz Pur Rahnama <sija@sija.pl>
2021-06-09 16:17:16 +0200

    Few subtle cleanups and improvements in parser code

--- src/parsers/parallel.cr
+++ src/parsers/parallel.cr
@@ -14,7 +14,7 @@ module Mint
           opening_bracket: ParallelExpectedOpeningBracket,
           closing_bracket: ParallelExpectedClosingBracket
         ) do
-          results = many { statement(:parallel) || comment }
+          results = many { statement(:sequence) || comment }
 
           raise ParallelExpectedStatement if results.none?(Ast::Statement)
 
