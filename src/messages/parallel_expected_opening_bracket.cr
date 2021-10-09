5b2a5d26fc85d846654b8798bca00813362bc064
Gusztáv Szikszai <guszti5@hotmail.com>
2021-10-09 09:38:33 +0200

    Remove parallel language feature.

--- src/messages/parallel_expected_opening_bracket.cr
+++
@@ -1,7 +0,0 @@
-message ParallelExpectedOpeningBracket do
-  title "Syntax Error"
-
-  opening_bracket "parallel expression", got
-
-  snippet node
-end
--- src/type_checkers/variable.cr
+++ src/type_checkers/variable.cr
@@ -7,7 +7,7 @@ module Mint
       %w(break case catch class const continue debugger default delete do else
         export extends finally for function if import in instanceof new return
         super switch this throw try typeof var void while with yield state
-        sequence parallel)
+        sequence)
 
     def check(node : Ast::Variable) : Checkable
       raise VariableReserved, {
