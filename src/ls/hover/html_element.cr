66f3ea9acfd9e987ab55ac5a79e579e13894ad40
Gusztáv Szikszai <guszti5@hotmail.com>
2021-10-13 09:24:55 +0200

    Use encode_path instead of encode in Crystal 1.2.0

--- src/ls/hover/html_element.cr
+++ src/ls/hover/html_element.cr
@@ -2,9 +2,16 @@ module Mint
   module LS
     class Hover < LSP::RequestMessage
       def hover(node : Ast::HtmlElement, workspace) : Array(String)
+        path =
+          {% if compare_versions(Crystal::VERSION, "1.1.1") >= 0 %}
+            URI.encode_path(node.tag.value)
+          {% else %}
+            URI.encode(node.tag.value, space_to_plus: true)
+          {% end %}
+
         [
           "**#{node.tag.value}**\n",
-          "[MDN Docs](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/#{URI.encode(node.tag.value, space_to_plus: true)})",
+          "[MDN Docs](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/#{path})",
         ]
       end
     end
