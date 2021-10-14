2637c421e564a88a055421e7ecf52419c6ead4d9
Gusztáv Szikszai <guszti5@hotmail.com>
2021-10-14 13:10:41 +0200

    Remove not used method.

--- src/parsers/block.cr
+++ src/parsers/block.cr
@@ -1,19 +1,5 @@
 module Mint
   class Parser
-    def block_with_comments(opening_bracket : SyntaxError.class,
-                            closing_bracket : SyntaxError.class)
-      block(opening_bracket, closing_bracket) do
-        head_comments = many { comment }
-        whitespace
-
-        result = yield
-        whitespace
-
-        tail_comments = many { comment }
-        {head_comments, result, tail_comments}
-      end
-    end
-
     def block(opening_bracket : SyntaxError.class,
               closing_bracket : SyntaxError.class)
       whitespace
