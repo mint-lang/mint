6fbc179f4b8ac9603106a68b6b184b7be48030aa
Szikszai Gusztáv <guszti5@hotmail.com>
2021-11-19 14:25:48 +0100

    [LSP] Basic HTML and CSS completions (#517)

--- src/ls/completion.cr
+++ src/ls/completion.cr
@@ -1,6 +1,19 @@
 module Mint
   module LS
     class Completion < LSP::RequestMessage
+      HTML_TAG_COMPLETIONS =
+        {{ read_file("#{__DIR__}/../assets/html_tags").strip }}
+          .lines
+          .map do |name|
+            LSP::CompletionItem.new(
+              kind: LSP::CompletionItemKind::Snippet,
+              insert_text: "<#{name}>${0}</#{name}>",
+              detail: "HTML Tag",
+              filter_text: name,
+              sort_text: name,
+              label: name)
+          end
+
       property params : LSP::CompletionParams
 
       def completions(node : Ast::Node, global : Bool = false)
@@ -29,7 +42,10 @@ module Mint
             .components
             .map { |node| completion_item(node) }
 
-        (global_completions + component_completions + scope_completions)
+        (global_completions +
+          component_completions +
+          scope_completions +
+          HTML_TAG_COMPLETIONS)
           .compact
           .sort_by!(&.label)
       end
