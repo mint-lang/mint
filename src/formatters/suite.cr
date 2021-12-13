d9542eb89361111e244ade443e22f360508331fd
Gusztáv Szikszai <guszti5@hotmail.com>
2021-12-13 09:30:57 +0100

    Fix formatting on nighly Crystal.

--- src/formatters/suite.cr
+++ src/formatters/suite.cr
@@ -2,7 +2,7 @@ module Mint
   class Formatter
     def format(node : Ast::Suite) : String
       body =
-        list (node.constants + node.tests + node.comments)
+        list(node.constants + node.tests + node.comments)
 
       name =
         format node.name
