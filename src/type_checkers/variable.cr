82d33cee9da4c4aac20f92bd0be7d6b03b5b82c1
Gusztáv Szikszai <guszti5@hotmail.com>
2022-01-14 10:59:34 +0000

    Allow function as a variable name.

--- src/type_checkers/variable.cr
+++ src/type_checkers/variable.cr
@@ -5,8 +5,8 @@ module Mint
 
     RESERVED =
       %w(break case catch class const continue debugger default delete do else
-        export extends finally for function if import in instanceof new return
-        super switch this throw try typeof var void while with yield state
+        export extends finally for if import in instanceof new return super
+        switch this throw try typeof var void while with yield state
         sequence parallel)
 
     def check(node : Ast::Variable) : Checkable
