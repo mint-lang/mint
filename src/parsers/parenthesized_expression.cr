e604ab15632c964c17f74b01bbdbfd6519102f85
Ary Borenszweig <asterite@gmail.com>
2022-02-24 20:35:09 -0300

    Parser: don't raise on failing to parse parenthesized expression

--- src/parsers/parenthesized_expression.cr
+++ src/parsers/parenthesized_expression.cr
@@ -4,14 +4,36 @@ module Mint
     syntax_error ParenthesizedExpressionExpectedExpression
 
     def parenthesized_expression : Ast::ParenthesizedExpression?
+      parenthesized_expression(error: true)
+    end
+
+    def parenthesized_expression_no_error : Ast::ParenthesizedExpression?
+      parenthesized_expression(error: false)
+    end
+
+    private def parenthesized_expression(error : Bool) : Ast::ParenthesizedExpression?
       start do |start_position|
         next unless char! '('
 
         whitespace
-        expression = expression! ParenthesizedExpressionExpectedExpression
+        expression = self.expression
+        unless expression
+          if error
+            raise ParenthesizedExpressionExpectedExpression
+          else
+            next
+          end
+        end
+
         whitespace
 
-        char ')', ParenthesizedExpressionExpectedClosingParentheses
+        unless char! ')'
+          if error
+            raise ParenthesizedExpressionExpectedClosingParentheses
+          else
+            next
+          end
+        end
 
         self << Ast::ParenthesizedExpression.new(
           expression: expression,
