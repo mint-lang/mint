0eea575a31a2740bbedfb7d8e807764ca95c9d5e
Szikszai Gusztáv <guszti5@hotmail.com>
2023-06-14 12:58:11 +0200

    Implement HTML highlighting.

--- src/commands/highlight.cr
+++ src/commands/highlight.cr
@@ -3,70 +3,17 @@ module Mint
     class Highlight < Admiral::Command
       include Command
 
-      define_help description: "Returns the syntax highlighted version of the given file as HTML"
+      define_help description: "Returns the syntax highlighted version of the given file"
 
-      define_argument path,
-        description: "The path to the file"
+      define_argument path, description: "The path to the file"
+
+      define_flag html : Bool,
+        description: "If specified, print the highlighted code as HTML",
+        default: false
 
       def run
         return unless path = arguments.path
-
-        ast =
-          Parser.parse(path)
-
-        tokenizer = SemanticTokenizer.new
-        tokenizer.tokenize(ast)
-
-        parts = [] of String | Tuple(String, SemanticTokenizer::TokenType)
-        contents = File.read(path)
-        position = 0
-
-        tokenizer.tokens.sort_by(&.from).each do |token|
-          if token.from > position
-            parts << contents[position, token.from - position]
-          end
-
-          parts << {contents[token.from, token.to - token.from], token.type}
-          position = token.to
-        end
-
-        if position < contents.size
-          parts << contents[position, contents.size]
-        end
-
-        result = parts.reduce("") do |memo, item|
-          memo + case item
-          in String
-            item
-          in Tuple(String, SemanticTokenizer::TokenType)
-            case item[1]
-            in .type?
-              item[0].colorize(:yellow)
-            in .type_parameter?
-              item[0].colorize(:light_yellow)
-            in .variable?
-              item[0].colorize(:dark_gray)
-            in .namespace?
-              item[0].colorize(:light_blue)
-            in .keyword?
-              item[0].colorize(:magenta)
-            in .property?
-              item[0].colorize(:dark_gray).mode(:underline)
-            in .comment?
-              item[0].colorize(:light_gray)
-            in .string?
-              item[0].colorize(:green)
-            in .number?
-              item[0].colorize(:red)
-            in .regexp?
-              item[0].colorize.fore(:white).back(:red)
-            in .operator?
-              item[0].colorize(:light_magenta)
-            end.to_s
-          end
-        end
-
-        print result
+        print SemanticTokenizer.highlight(path, flags.html)
       end
     end
   end
--- src/semantic_tokenizer.cr
+++ src/semantic_tokenizer.cr
@@ -25,6 +25,7 @@ module Mint
       Ast::Variable      => TokenType::Variable,
       Ast::Comment       => TokenType::Comment,
       Ast::StringLiteral => TokenType::String,
+      Ast::RegexpLiteral => TokenType::Regexp,
       Ast::NumberLiteral => TokenType::Number,
       Ast::TypeId        => TokenType::Type,
     }
@@ -41,6 +42,71 @@ module Mint
 
     # This is where the resulting tokens are stored.
     getter tokens : Array(Token) = [] of Token
+
+    def self.highlight(path : String, html : Bool = false)
+      ast =
+        Parser.parse(path)
+
+      tokenizer = self.new
+      tokenizer.tokenize(ast)
+
+      parts = [] of String | Tuple(String, SemanticTokenizer::TokenType)
+      contents = ast.nodes.first.input.input
+      position = 0
+
+      tokenizer.tokens.sort_by(&.from).each do |token|
+        if token.from > position
+          parts << contents[position, token.from - position]
+        end
+
+        parts << {contents[token.from, token.to - token.from], token.type}
+        position = token.to
+      end
+
+      if position < contents.size
+        parts << contents[position, contents.size]
+      end
+
+      parts.reduce("") do |memo, item|
+        memo + case item
+        in String
+          if html
+            HTML.escape(item)
+          else
+            item
+          end
+        in Tuple(String, SemanticTokenizer::TokenType)
+          if html
+            "<span class=\"#{item[1].to_s.underscore}\">#{HTML.escape(item[0])}</span>"
+          else
+            case item[1]
+            in .type?
+              item[0].colorize(:yellow)
+            in .type_parameter?
+              item[0].colorize(:light_yellow)
+            in .variable?
+              item[0].colorize(:dark_gray)
+            in .namespace?
+              item[0].colorize(:light_blue)
+            in .keyword?
+              item[0].colorize(:magenta)
+            in .property?
+              item[0].colorize(:dark_gray).mode(:underline)
+            in .comment?
+              item[0].colorize(:light_gray)
+            in .string?
+              item[0].colorize(:green)
+            in .number?
+              item[0].colorize(:red)
+            in .regexp?
+              item[0].colorize(:light_red)
+            in .operator?
+              item[0].colorize(:light_magenta)
+            end.to_s
+          end
+        end
+      end
+    end
 
     def tokenize(ast : Ast)
       # We add the operators and keywords directly from the AST
