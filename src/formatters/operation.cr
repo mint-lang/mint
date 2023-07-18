6e51ae076ad442fb72e97ff5bd6ec42343b52ab3
Szikszai Gusztáv <guszti5@hotmail.com>
2023-07-18 16:49:27 +0200

    Update operation.cr

--- src/formatters/operation.cr
+++ src/formatters/operation.cr
@@ -7,8 +7,7 @@ module Mint
       right =
         format node.right
 
-      if node.new_line? &&
-         node.right.is_a?(Ast::Operation)
+      if node.new_line?
         "#{left} #{node.operator}\n#{indent(right.remove_all_leading_whitespace)}"
       else
         "#{left} #{node.operator} #{right}"
