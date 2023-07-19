cab06c7b8ea54b1702419f8f8ad766899189c6d9
Szikszai Gusztáv <guszti5@hotmail.com>
2023-07-19 06:10:58 +0200

    Apply suggestions from code review
    Co-authored-by: Sijawusz Pur Rahnama <sija@sija.pl>

--- src/lsp/protocol/semantic_tokens_client_capabilities.cr
+++ src/lsp/protocol/semantic_tokens_client_capabilities.cr
@@ -8,7 +8,7 @@ module LSP
 
     # The token types that the client supports.
     @[JSON::Field(key: "tokenTypes")]
-    property token_types : Array(String)
+    property token_types : Array(String)?
 
     def initialize(@dynamic_registration = nil, @token_types = nil)
     end
