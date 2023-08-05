cad3b2843ce72ac1fef123cfe98c98bd330bc8d4
Jon Sully <jon@sul.ly>
2023-08-05 14:28:41 +0100

    LSP: Support linking to multiple Module locations

--- src/ls/definition/type_id.cr
+++ src/ls/definition/type_id.cr
@@ -3,22 +3,29 @@ module Mint
     class Definition < LSP::RequestMessage
       def definition(node : Ast::TypeId, server : Server, workspace : Workspace, stack : Array(Ast::Node))
         found =
-          workspace.ast.enums.find(&.name.value.==(node.value)) ||
-            workspace.ast.records.find(&.name.value.==(node.value)) ||
-            workspace.ast.stores.find(&.name.value.==(node.value)) ||
-            workspace.ast.modules.find(&.name.value.==(node.value)) ||
-            find_component(workspace, node.value)
+          workspace.ast.enums.select(&.name.value.==(node.value)) +
+            workspace.ast.records.select(&.name.value.==(node.value)) +
+            workspace.ast.stores.select(&.name.value.==(node.value)) +
+            workspace.ast.components.select(&.name.value.==(node.value)) +
+            workspace.ast.modules.select(&.name.value.==(node.value))
 
-        if found.nil? && (next_node = stack[1])
+        if found.empty? && (next_node = stack[1]?)
           return definition(next_node, server, workspace, stack)
         end
 
-        return if Core.ast.nodes.includes?(found)
+        links = found
+          .reject(&.in?(Core.ast.nodes))
+          .map do |mod|
+            case mod
+            when Ast::Store, Ast::Enum, Ast::Component, Ast::RecordDefinition, Ast::Module
+              location_link server, node, mod.name, mod
+            end
+          end
+          .compact
 
-        case found
-        when Ast::Store, Ast::Enum, Ast::Component, Ast::RecordDefinition, Ast::Module
-          location_link server, node, found.name, found
-        end
+        return links if links.size > 1
+
+        links.first?
       end
     end
   end
