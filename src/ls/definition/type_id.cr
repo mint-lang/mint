53e80330231fa8c6888e79081f1e7095fef2f6f8
Jon Sully <jon@sul.ly>
2023-08-05 15:57:04 +0100

    LSP: Fix issues where modules share the same name as an Enum/Record

--- src/ls/definition/type_id.cr
+++ src/ls/definition/type_id.cr
@@ -2,30 +2,34 @@ module Mint
   module LS
     class Definition < LSP::RequestMessage
       def definition(node : Ast::TypeId, server : Server, workspace : Workspace, stack : Array(Ast::Node))
-        found =
-          workspace.ast.enums.select(&.name.value.==(node.value)) +
-            workspace.ast.records.select(&.name.value.==(node.value)) +
-            workspace.ast.stores.select(&.name.value.==(node.value)) +
-            workspace.ast.components.select(&.name.value.==(node.value)) +
-            workspace.ast.modules.select(&.name.value.==(node.value))
+        case stack[1]?
+        when Ast::ModuleAccess
+          links = workspace.ast.modules.select(&.name.value.==(node.value))
+            .reject(&.in?(Core.ast.nodes))
+            .map do |mod|
+              location_link server, node, mod.name, mod
+            end
 
-        if found.empty? && (next_node = stack[1]?)
+          return links.first if links.size == 1
+          return links unless links.empty?
+        end
+
+        found =
+          workspace.ast.enums.find(&.name.value.==(node.value)) ||
+            workspace.ast.records.find(&.name.value.==(node.value)) ||
+            workspace.ast.stores.find(&.name.value.==(node.value)) ||
+            find_component(workspace, node.value)
+
+        if found.nil? && (next_node = stack[1])
           return definition(next_node, server, workspace, stack)
         end
 
-        links = found
-          .reject(&.in?(Core.ast.nodes))
-          .map do |mod|
-            case mod
-            when Ast::Store, Ast::Enum, Ast::Component, Ast::RecordDefinition, Ast::Module
-              location_link server, node, mod.name, mod
-            end
-          end
-          .compact
+        return if Core.ast.nodes.includes?(found)
 
-        return links if links.size > 1
-
-        links.first?
+        case found
+        when Ast::Store, Ast::Enum, Ast::Component, Ast::RecordDefinition
+          location_link server, node, found.name, found
+        end
       end
     end
   end
