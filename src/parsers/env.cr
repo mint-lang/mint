59e22580166a9224a5fc0a3b5f939b2cdbcaf853
Szikszai Gusztáv <guszti5@hotmail.com>
2023-08-09 16:38:57 +0200

    Convert env parser errors.

--- src/parsers/env.cr
+++ src/parsers/env.cr
@@ -1,7 +1,5 @@
 module Mint
   class Parser
-    syntax_error EnvExpectedName
-
     def env : Ast::Env?
       start do |start_position|
         next unless char! '@'
@@ -15,7 +13,10 @@ module Mint
         name =
           "#{head}#{tail}"
 
-        raise EnvExpectedName if name.empty?
+        next error :env_expected_name do
+          expected "the name of the environment variable", word
+          snippet self
+        end unless name
 
         self << Ast::Env.new(
           from: start_position,
