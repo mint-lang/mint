2db42a33d7ddcb624b81892225efaab277c30baa
Szikszai Gusztáv <guszti5@hotmail.com>
2023-08-09 15:04:27 +0200

    Convert css keyframes parser errors.

--- src/messages/css_keyframes_expected_closing_bracket.cr
+++
@@ -1,7 +0,0 @@
-message CssKeyframesExpectedClosingBracket do
-  title "Syntax Error"
-
-  closing_bracket "keyframes", got
-
-  snippet node
-end
--- src/parsers/css_keyframes.cr
+++ src/parsers/css_keyframes.cr
@@ -1,9 +1,5 @@
 module Mint
   class Parser
-    syntax_error CssKeyframesExpectedOpeningBracket
-    syntax_error CssKeyframesExpectedClosingBracket
-    syntax_error CssKeyframesExpectedName
-
     def css_keyframes : Ast::CssKeyframes?
       start do |start_position|
         next unless keyword "@keyframes"
@@ -13,13 +9,27 @@ module Mint
         name =
           gather { chars_until '{' }.presence.try(&.strip)
 
-        raise CssKeyframesExpectedName unless name
+        next error :css_keyframes_expected_name do
+          expected "the name of a CSS keyframes rule", word
+          snippet self
+        end unless name
 
-        selectors = block(
-          opening_bracket: CssKeyframesExpectedOpeningBracket,
-          closing_bracket: CssKeyframesExpectedClosingBracket) do
+        selectors = block2(
+          ->{ error :css_keyframes_expected_opening_bracket do
+            expected "the opening bracket of a CSS keyframes rule", word
+            snippet self
+          end },
+          ->{ error :css_keyframes_expected_closing_bracket do
+            expected "the closing bracket of a CSS keyframes rule", word
+            snippet self
+          end }) do
           many { comment || css_selector(only_definitions: true) }
         end
+
+        next error :css_keyframes_expected_selectors do
+          expected "the selectors of a CSS keyframes rule", word
+          snippet self
+        end if selectors.empty?
 
         Ast::CssKeyframes.new(
           from: start_position,
