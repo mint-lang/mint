674e60862f962c3c2fc06cd3333b49d98ef39bc3
Szikszai Gusztáv <guszti5@hotmail.com>
2023-08-09 16:35:28 +0200

    Convert encode parser errors.

--- src/parsers/encode.cr
+++ src/parsers/encode.cr
@@ -1,14 +1,22 @@
 module Mint
   class Parser
-    syntax_error EncodeExpectedExpression
-
     def encode : Ast::Encode?
       start do |start_position|
         next unless keyword "encode"
         next unless whitespace?
         whitespace
 
-        expression = expression! EncodeExpectedExpression
+        next error :encode_expected_expression do
+          block do
+            text "The"
+            bold "object to be encoded"
+            text "must come from an"
+            bold "expression."
+          end
+
+          expected "the expression", word
+          snippet self
+        end unless expression = self.expression
 
         self << Ast::Encode.new(
           expression: expression,
