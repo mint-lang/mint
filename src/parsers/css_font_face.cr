7459c4fffd6411c77468afcb0c7f384f3f4aa855
Szikszai Gusztáv <guszti5@hotmail.com>
2023-08-09 14:56:03 +0200

    Convert css font-face parser errors.

--- src/parsers/css_font_face.cr
+++ src/parsers/css_font_face.cr
@@ -1,17 +1,25 @@
 module Mint
   class Parser
-    syntax_error CssFontFaceExpectedOpeningBracket
-    syntax_error CssFontFaceExpectedClosingBracket
-
     def css_font_face : Ast::CssFontFace?
       start do |start_position|
         next unless keyword "@font-face"
 
-        definitions = block(
-          opening_bracket: CssFontFaceExpectedOpeningBracket,
-          closing_bracket: CssFontFaceExpectedClosingBracket) do
+        definitions = block2(
+          ->{ error :css_font_face_expected_opening_bracket do
+            expected "the opening bracket of a CSS font-face rule", word
+            snippet self
+          end },
+          ->{ error :css_font_face_expected_closing_bracket do
+            expected "the closing bracket of a CSS font-face rule", word
+            snippet self
+          end }) do
           many { comment || css_definition }
         end
+
+        next error :css_font_face_expected_definitions do
+          expected "the definitions of a CSS font-face rule", word
+          snippet self
+        end if definitions.empty?
 
         Ast::CssFontFace.new(
           definitions: definitions,
