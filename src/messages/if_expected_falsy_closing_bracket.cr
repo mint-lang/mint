bab4d8a0c9ae84bd21d38766d6c5947bf4a8abff
Szikszai Gusztáv <guszti5@hotmail.com>
2023-08-10 14:18:39 +0200

    Convert if expression parser errors.

--- src/messages/if_expected_falsy_closing_bracket.cr
+++
@@ -1,7 +0,0 @@
-message IfExpectedFalsyClosingBracket do
-  title "Syntax Error"
-
-  closing_bracket "else branch", got
-
-  snippet node
-end
--- src/parsers/if.cr
+++ src/parsers/if.cr
@@ -1,41 +1,95 @@
 module Mint
   class Parser
-    syntax_error IfExpectedTruthyOpeningBracket
-    syntax_error IfExpectedTruthyClosingBracket
-    syntax_error IfExpectedFalsyOpeningBracket
-    syntax_error IfExpectedFalsyClosingBracket
-    syntax_error IfExpectedClosingParentheses
-    syntax_error IfExpectedTruthyExpression
-    syntax_error IfExpectedFalsyExpression
-    syntax_error IfExpectedCondition
-
     def if_expression(for_css = false) : Ast::If?
+      thruthy_opening_error = ->{ error :if_expected_truthy_opening_bracket do
+        expected "the opening bracket of the truthy expression", word
+        snippet self
+      end }
+
+      thruthy_closing_error = ->{ error :if_expected_truthy_closing_bracket do
+        expected "the closing bracket of the truthy expression", word
+        snippet self
+      end }
+
+      thruthy_error = ->{ error :if_expected_truthy_expression do
+        block do
+          text "The"
+          bold "body of an if expression"
+          text "must be a single expression."
+        end
+
+        expected "an expression", word
+        snippet self
+      end }
+
+      else_opening_error = ->{ error :if_expected_else_opening_bracket do
+        expected "the opening bracket of the else expression", word
+        snippet self
+      end }
+
+      else_closing_error = ->{ error :if_expected_else_closing_bracket do
+        expected "the closing bracket of the else expression", word
+        snippet self
+      end }
+
+      else_error = ->{ error :if_expected_else_expression do
+        block do
+          text "The"
+          bold "body of an if expression"
+          text "must be a single expression."
+        end
+
+        expected "an expression", word
+        snippet self
+      end }
+
       start do |start_position|
         next unless keyword "if"
 
         whitespace
         parens = char! '('
+
         whitespace
-        condition = statement || expression!(IfExpectedCondition)
+        next error :if_expected_condition do
+          block do
+            text "The"
+            bold "condition"
+            text "of an if expression"
+            bold "must be a single expression."
+          end
+
+          expected "the condition", word
+          snippet self
+        end unless condition = statement || expression
+
         whitespace
-        char ')', IfExpectedClosingParentheses if parens
+        next error :if_expected_closing_parenthesis do
+          block do
+            text "The"
+            bold "condition"
+            text "of an"
+            bold "if expression"
+            text "must be enclosed by parenthesis."
+          end
+
+          expected "the closing parenthesis", word
+          snippet self
+        end if parens && !char!(')')
         whitespace
 
         truthy =
           if for_css
-            block(
-              opening_bracket: IfExpectedTruthyOpeningBracket,
-              closing_bracket: IfExpectedTruthyClosingBracket) do
+            block2(thruthy_opening_error, thruthy_closing_error) do
               many { css_definition }
             end
           else
-            code_block(
-              opening_bracket: IfExpectedTruthyOpeningBracket,
-              closing_bracket: IfExpectedTruthyClosingBracket,
-              statement_error: IfExpectedTruthyExpression)
+            code_block2(
+              opening_bracket_error: thruthy_opening_error,
+              closing_bracket_error: thruthy_closing_error,
+              statement_error: thruthy_error)
           end
 
-        raise IfExpectedTruthyExpression unless truthy
+        thruthy_error.call unless truthy
 
         falsy = nil
         whitespace
@@ -46,19 +100,17 @@ module Mint
           unless falsy = if_expression(for_css: for_css)
             falsy =
               if for_css
-                block(
-                  opening_bracket: IfExpectedFalsyOpeningBracket,
-                  closing_bracket: IfExpectedFalsyClosingBracket) do
+                block2(else_opening_error, else_closing_error) do
                   many { css_definition }
                 end
               else
-                code_block(
-                  opening_bracket: IfExpectedFalsyOpeningBracket,
-                  closing_bracket: IfExpectedFalsyClosingBracket,
-                  statement_error: IfExpectedFalsyExpression)
+                code_block2(
+                  opening_bracket_error: else_opening_error,
+                  closing_bracket_error: else_closing_error,
+                  statement_error: else_error)
               end
 
-            raise IfExpectedFalsyExpression unless falsy
+            else_error.call unless falsy
           end
         end
 
