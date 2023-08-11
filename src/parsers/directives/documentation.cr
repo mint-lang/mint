30ffa6fef9957430479b0f3fef8a82c73d4e7420
Szikszai Gusztáv <guszti5@hotmail.com>
2023-08-11 16:12:18 +0200

    Convert documentation directive parser errors.

--- src/parsers/directives/documentation.cr
+++ src/parsers/directives/documentation.cr
@@ -1,18 +1,25 @@
 module Mint
   class Parser
-    syntax_error DocumentationDirectiveExpectedClosingParentheses
-    syntax_error DocumentationDirectiveExpectedOpeningParentheses
-    syntax_error DocumentationDirectiveExpectedEntity
-
     def documentation_directive : Ast::Directives::Documentation?
       start do |start_position|
         next unless keyword "@documentation"
 
-        char '(', DocumentationDirectiveExpectedOpeningParentheses
+        next error :documentation_directive_expected_opening_parenthesis do
+          expected "the opening parenthesis of a documentation directive", word
+          snippet self
+        end unless char! '('
         whitespace
-        entity = type_id! DocumentationDirectiveExpectedEntity
+
+        next error :documentation_directive_expected_entity do
+          expected "the entity of a documentation directive", word
+          snippet self
+        end unless entity = type_id
         whitespace
-        char ')', DocumentationDirectiveExpectedClosingParentheses
+
+        next error :documentation_directive_expected_closing_parenthesis do
+          expected "the closing parenthesis of a documentation directive", word
+          snippet self
+        end unless char! ')'
 
         self << Ast::Directives::Documentation.new(
           from: start_position,
