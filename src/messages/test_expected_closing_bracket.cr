6a9c6db06df38e593fb8aba6aaeb9df7ec158a2d
Szikszai Gusztáv <guszti5@hotmail.com>
2023-08-11 14:44:04 +0200

    Convert test parser errors.

--- src/messages/test_expected_closing_bracket.cr
+++
@@ -1,7 +0,0 @@
-message TestExpectedClosingBracket do
-  title "Syntax Error"
-
-  closing_bracket "test", got
-
-  snippet node
-end
--- src/parsers/test.cr
+++ src/parsers/test.cr
@@ -1,26 +1,32 @@
 module Mint
   class Parser
-    syntax_error TestExpectedOpeningBracket
-    syntax_error TestExpectedClosingBracket
-    syntax_error TestExpectedExpression
-    syntax_error TestExpectedName
-
     def test : Ast::Test?
       start do |start_position|
         next unless keyword "test"
 
         whitespace
 
-        name = string_literal! TestExpectedName,
-          with_interpolation: false
+        next error :test_expected_name do
+          expected "the name of a test", word
+          snippet self
+        end unless name = string_literal with_interpolation: false
 
         whitespace
 
         expression =
-          code_block(
-            opening_bracket: TestExpectedOpeningBracket,
-            closing_bracket: TestExpectedClosingBracket,
-            statement_error: TestExpectedExpression)
+          code_block2(
+            ->{ error :test_expected_opening_bracket do
+              expected "the opening bracket of a test", word
+              snippet self
+            end },
+            ->{ error :test_expected_closing_bracket do
+              expected "the closing bracket of a test", word
+              snippet self
+            end },
+            ->{ error :test_expected_body do
+              expected "the body of a test", word
+              snippet self
+            end })
 
         self << Ast::Test.new(
           expression: expression,
