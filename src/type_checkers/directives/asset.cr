158a5e2c274f3cef407d44d5d29b82c15dd1d536
Szikszai Gusztáv <guszti5@hotmail.com>
2023-08-12 13:45:58 +0200

    Convert asset directive type checking errors.

--- src/type_checkers/directives/asset.cr
+++ src/type_checkers/directives/asset.cr
@@ -1,12 +1,16 @@
 module Mint
   class TypeChecker
-    type_error AssetDirectiveExpectedFile
-
     def check(node : Ast::Directives::Asset) : Checkable
-      raise AssetDirectiveExpectedFile, {
-        "path" => node.real_path.to_s,
-        "node" => node,
-      } unless node.exists?
+      error :asset_directive_expected_file do
+        block "The path specified for an asset directive does not exists."
+
+        block do
+          text "The file should be here: "
+          bold node.real_path.to_s
+        end
+
+        snippet node
+      end unless node.exists?
 
       assets << node if checking?
       STRING
