6ebb2d5586016d2fb39061e0175621729c0c0ca2
Szikszai Gusztáv <guszti5@hotmail.com>
2023-08-12 14:04:52 +0200

    Convert call type checking errors.

--- src/type_checkers/call.cr
+++ src/type_checkers/call.cr
@@ -1,12 +1,5 @@
 module Mint
   class TypeChecker
-    type_error CallArgumentSizeMismatch
-    type_error CallArgumentTypeMismatch
-    type_error CallNotFoundArgument
-    type_error CallMixedArguments
-    type_error CallTypeMismatch
-    type_error CallNotAFunction
-
     def check(node : Ast::CallExpression)
       resolve(node.expression).dup.tap do |item|
         item.label = node.name.try(&.value)
@@ -21,9 +14,10 @@ module Mint
     end
 
     def check_call(node, function_type) : Checkable
-      raise CallNotAFunction, {
-        "node" => node,
-      } unless function_type.name == "Function"
+      return error :call_not_a_function do
+        snippet "The entity you called is not a function, instead it is:", function_type
+        snippet "The call is here:", node
+      end unless function_type.name == "Function"
 
       argument_size =
         function_type.parameters.size - 1
@@ -39,12 +33,17 @@ module Mint
       parameters =
         [] of Checkable
 
-      raise CallArgumentSizeMismatch, {
-        "call_size" => node.arguments.size.to_s,
-        "size"      => argument_size.to_s,
-        "node"      => node,
-      } if node.arguments.size > argument_size ||       # If it's more than the maximum
-           node.arguments.size < required_argument_size # If it's less then the minimum
+      error :call_argument_size_mismatch do
+        block do
+          text "The function you called takes"
+          bold argument_size.to_s
+          text "arguments, while you tried to call it with"
+          bold node.arguments.size.to_s
+        end
+
+        snippet "You tried to call it here:", node
+      end if node.arguments.size > argument_size ||       # If it's more than the maximum
+             node.arguments.size < required_argument_size # If it's less then the minimum
 
       args =
         if node.arguments.all?(&.name.nil?)
@@ -56,18 +55,25 @@ module Mint
                 .parameters
                 .index { |param| param.label == argument.name.try(&.value) }
 
-            raise CallNotFoundArgument, {
-              "function_type" => function_type,
-              "name"          => argument.name.try(&.value).to_s,
-              "node"          => node,
-            } unless index
+            error :call_not_found_argument do
+              block do
+                text "I was looking for the argument:"
+                bold argument.name.try(&.value).to_s
+                text "but it's not there."
+              end
+
+              snippet "The type of the function is:", function_type
+              snippet "The call is here:", node
+            end unless index
 
             index
           end
         else
-          raise CallMixedArguments, {
-            "node" => node,
-          }
+          error :call_mixed_arguments do
+            block "A call cannot have named and unamed arguments at the same time."
+
+            snippet "It is here:", node
+          end
         end
 
       argument_order.concat args
@@ -79,13 +85,20 @@ module Mint
         function_argument_type =
           function_type.parameters[index]
 
-        raise CallArgumentTypeMismatch, {
-          "index"    => ordinal(index + 1),
-          "expected" => function_argument_type,
-          "got"      => argument_type,
-          "function" => function_type,
-          "node"     => node,
-        } unless Comparer.compare(function_argument_type, argument_type)
+        error :call_argument_type_mismatch do
+          ordinal =
+            ordinal(index + 1)
+
+          block do
+            text "The"
+            bold "#{ordinal} argument"
+            text "to a function is causing a mismatch."
+          end
+
+          snippet "The function is expecting the #{ordinal} argument to be:", function_argument_type
+          snippet "Instead it is:", argument_type
+          snippet "You tried to call it here:", node
+        end unless Comparer.compare(function_argument_type, argument_type)
 
         parameters << argument_type
       end
@@ -100,11 +113,16 @@ module Mint
       result =
         Comparer.compare(function_type, call_type)
 
-      raise CallTypeMismatch, {
-        "expected" => function_type,
-        "got"      => call_type,
-        "node"     => node,
-      } unless result
+      error :call_type_mismatch do
+        block do
+          text "The type signature of the call does not match the signature"
+          text "of the function."
+        end
+
+        snippet "The type signature of the function is:", function_type
+        snippet "You tried to call it as:", call_type
+        snippet "You tried to call it here:", node
+      end unless result
 
       resolve_type(result.parameters.last)
     end
