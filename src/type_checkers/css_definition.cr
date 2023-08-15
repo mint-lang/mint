05d8e33c7f176cb52d93b43807c1be65dfacf32a
Szikszai Gusztáv <guszti5@hotmail.com>
2023-08-15 13:27:44 +0200

    Convert css definition type checking errors.

--- src/type_checkers/css_definition.cr
+++ src/type_checkers/css_definition.cr
@@ -5,9 +5,6 @@ module Mint
         .strip
         .lines
 
-    type_error CssDefinitionTypeMismatch
-    type_error CssNoProperty
-
     def check(node : Ast::CssDefinition) : Checkable
       node.value.each do |item|
         type =
@@ -19,17 +16,31 @@ module Mint
           end
 
         unless node.name.starts_with?('-')
-          raise CssNoProperty, {
-            "name" => node.name,
-            "node" => node,
-          } unless CSS_PROPERTY_NAMES.includes?(node.name)
+          error :css_definition_no_property do
+            block do
+              text "There is no CSS property with the name:"
+              code node.name
+            end
+
+            snippet node
+          end unless CSS_PROPERTY_NAMES.includes?(node.name)
         end
 
-        raise CssDefinitionTypeMismatch, {
-          "name" => node.name,
-          "node" => node,
-          "got"  => type,
-        } unless Comparer.matches_any?(type, [STRING, NUMBER])
+        error :css_definition_type_mismatch do
+          block do
+            text "The type of the value for the CSS property"
+            bold node.name
+            text "is invalid."
+          end
+
+          block do
+            text "I was expecting one of these types:"
+          end
+
+          snippet "String, Number"
+          snippet "Instead it is:", type
+          snippet node
+        end unless Comparer.matches_any?(type, [STRING, NUMBER])
       end
 
       VOID
