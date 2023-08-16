44cd08ca588807f1089b26ce18de8819734d730a
Szikszai Gusztáv <guszti5@hotmail.com>
2023-08-16 17:04:11 +0200

    Convert documentation directive type checking errors.

--- src/type_checkers/directives/documentation.cr
+++ src/type_checkers/directives/documentation.cr
@@ -1,15 +1,18 @@
 module Mint
   class TypeChecker
-    type_error DocumentationDirectiveEntityNotFound
-
     def check(node : Ast::Directives::Documentation) : Checkable
       component =
         ast.components.find(&.name.value.==(node.entity.value))
 
-      raise DocumentationDirectiveEntityNotFound, {
-        "name" => node.entity.value,
-        "node" => node,
-      } unless component
+      error :documentation_directive_entity_not_found do
+        block do
+          text "The entity for the documentation directive:"
+          code node.entity.value
+          text "does not exists."
+        end
+
+        snippet node
+      end unless component
 
       lookups[node] = component
 
