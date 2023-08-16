267eef0bd14aeb41433550898ea6be94c69f5cdf
Szikszai Gusztáv <guszti5@hotmail.com>
2023-08-16 21:35:37 +0200

    Convert  html content type checking errors.

--- src/type_checkers/html_content.cr
+++ src/type_checkers/html_content.cr
@@ -1,7 +1,5 @@
 module Mint
   class TypeChecker
-    type_error HtmlContentTypeMismatch
-
     def check_html(nodes : Array(Ast::Node)) : Checkable
       nodes.each do |child|
         type = resolve child
@@ -11,10 +9,14 @@ module Mint
            Comparer.compare(HTML_CHILDREN, type) ||
            Comparer.compare(TEXT_CHILDREN, type)
         else
-          raise HtmlContentTypeMismatch, {
-            "node" => child,
-            "got"  => type,
-          }
+          error :html_content_type_mismatch do
+            block "A child node of an element or component has an invalid type."
+            block "I was expecting one of the following types:"
+
+            snippet "Html, String, Array(String), Array(Html)"
+            snippet "Instead it is:", type
+            snippet child
+          end
         end
       end
 
