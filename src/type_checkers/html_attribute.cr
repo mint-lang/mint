7552a70591e2d8225e803ad405c02ab70ffc2e38
Szikszai Gusztáv <guszti5@hotmail.com>
2023-08-16 21:06:00 +0200

    Convert html attribute type checking errors.

--- src/type_checkers/html_attribute.cr
+++ src/type_checkers/html_attribute.cr
@@ -1,22 +1,21 @@
 module Mint
   class TypeChecker
-    type_error HtmlAttributeComponentPropertyTypeMismatch
-    type_error HtmlAttributeElementAttributeTypeMismatch
-    type_error HtmlAttributeNotFoundComponentProperty
-    type_error HtmlAttributeComponentKeyTypeMismatch
-    type_error HtmlAttributeFragmentKeyTypeMismatch
-    type_error HtmlElementClassNameForbidden
-    type_error HtmlElementRefForbidden
-
     def check(node : Ast::HtmlAttribute, element : Ast::HtmlFragment)
       got =
         resolve node.value
 
-      raise HtmlAttributeFragmentKeyTypeMismatch, {
-        "expected" => STRING,
-        "node"     => node,
-        "got"      => got,
-      } unless Comparer.compare(STRING, got)
+      error :html_attribute_fragment_key_type_mismatch do
+        block do
+          text "The"
+          bold "key"
+          text "attribute of a fragment has an invalid type. It can only be:"
+          code "String"
+        end
+
+        expected STRING, got
+
+        snippet node
+      end unless Comparer.compare(STRING, got)
 
       got
     end
@@ -28,30 +27,47 @@ module Mint
       expected =
         case node.name.value.downcase
         when "ref"
-          raise HtmlElementRefForbidden, {
-            "node" => node,
-          }
+          error :html_element_ref_forbidden do
+            block "The use of ref attribute is forbidden."
+            block "If you want to save a reference to an element, use the as keyword."
+            snippet node
+          end
         when .starts_with?("on")
           [EVENT_FUNCTION, VOID_FUNCTION]
         when "readonly", "disabled", "checked"
           [BOOL]
         when "className"
-          raise HtmlElementClassNameForbidden, {
-            "node" => node,
-          }
+          error :html_element_class_name_forbidden do
+            block "The className attribute on elements are forbidden."
+
+            block do
+              text "Please use"
+              bold "class"
+              text "instead."
+            end
+
+            snippet node
+          end
         when "style"
           [STYLE_MAP, STRING]
         else
           [STRING]
         end
 
-      raise HtmlAttributeElementAttributeTypeMismatch, {
-        "expected" => expected.join(", "),
-        "tag"      => element.tag.value,
-        "name"     => node.name.value,
-        "node"     => node,
-        "got"      => got,
-      } unless expected.any? { |item| Comparer.compare(item, got) }
+      error :html_attribute_element_attribute_type_mismatch do
+        block do
+          text "The type of the value for the property"
+          bold node.name.value
+          text "of element"
+          bold element.tag.value
+          text "does not match its type."
+        end
+
+        snippet "I was expecting one of the following types:", expected.join(", ")
+        snippet "Instead it is:", got
+
+        snippet node
+      end unless expected.any? { |item| Comparer.compare(item, got) }
 
       got
     end
@@ -61,24 +77,38 @@ module Mint
 
       case node.name.value
       when "key"
-        raise HtmlAttributeComponentKeyTypeMismatch, {
-          "component" => component.name,
-          "expected"  => STRING,
-          "got"       => type,
-          "node"      => node,
-        } unless Comparer.compare(type, STRING)
+        error :html_attribute_component_key_type_mismatch do
+          block do
+            text "The"
+            bold "key"
+            text "attribute of a component has an invalid type. It can only be:"
+            code "String"
+          end
+
+          expected STRING, type
+
+          snippet node
+        end unless Comparer.compare(type, STRING)
       else
         prop =
           component
             .properties
             .find(&.name.value.==(node.name.value))
 
-        raise HtmlAttributeNotFoundComponentProperty, {
-          "properties" => component.properties.map(&.name.value),
-          "name"       => node.name.value,
-          "component"  => component.name.value,
-          "node"       => node,
-        } unless prop
+        error :html_attribute_not_found_component_property do
+          block do
+            text "I was looking for the property"
+            bold node.name.value
+            text "on the"
+            bold component.name.value
+            text "component but could not find it."
+          end
+
+          snippet "Maybe you want one of its properties:",
+            component.properties.map(&.name.value).join("\n")
+
+          snippet node
+        end unless prop
 
         lookups[node] = prop
 
@@ -87,13 +117,19 @@ module Mint
             resolve prop
           end
 
-        raise HtmlAttributeComponentPropertyTypeMismatch, {
-          "name"      => prop.name.value,
-          "component" => component.name.value,
-          "expected"  => prop_type,
-          "node"      => node,
-          "got"       => type,
-        } unless Comparer.compare(prop_type, type)
+        error :html_attribute_component_property_type_mismatch do
+          block do
+            text "The type of the value for the property"
+            bold prop.name.value
+            text "of the component"
+            bold component.name.value
+            text "does not match its definition."
+          end
+
+          expected prop_type, type
+
+          snippet node
+        end unless Comparer.compare(prop_type, type)
       end
 
       type
