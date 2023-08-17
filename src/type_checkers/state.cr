939417f06da6c1091f24b135bf7cd5b35102d04d
Szikszai Gusztáv <guszti5@hotmail.com>
2023-08-17 13:51:17 +0200

    Convert state type checking errors.

--- src/type_checkers/state.cr
+++ src/type_checkers/state.cr
@@ -1,7 +1,5 @@
 module Mint
   class TypeChecker
-    type_error StateTypeMismatch
-
     def static_type_signature(node : Ast::State) : Checkable
       node.type.try { |type| resolve type } || Variable.new("a")
     end
@@ -19,12 +17,16 @@ module Mint
         resolved =
           Comparer.compare(type, default)
 
-        raise StateTypeMismatch, {
-          "expected" => type,
-          "name"     => node.name.value,
-          "node"     => node.default,
-          "got"      => default,
-        } unless resolved
+        error :state_type_mismatch do
+          block do
+            text "The type of the default value of the"
+            bold node.name.value
+            text "state does not match its type annotation."
+          end
+
+          expected type, default
+          snippet node.default
+        end unless resolved
 
         resolved
       else
