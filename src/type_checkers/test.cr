552566ba8929c73a527bea5f85f54c629da91355
Szikszai Gusztáv <guszti5@hotmail.com>
2023-08-17 14:30:48 +0200

    Convert test type checking errors.

--- src/type_checkers/test.cr
+++ src/type_checkers/test.cr
@@ -1,7 +1,5 @@
 module Mint
   class TypeChecker
-    type_error TestTypeMismatch
-
     def check(node : Ast::Test)
       resolve node.name
 
@@ -11,10 +9,14 @@ module Mint
       if Comparer.compare(type, BOOL) ||
          Comparer.compare(type, TEST_CONTEXT)
       else
-        raise TestTypeMismatch, {
-          "node" => node.expression,
-          "got"  => type,
-        }
+        error :test_type_mismatch do
+          block "The type of a test does not match any of the allowed types."
+          block "I was expecting one of:"
+
+          snippet "Bool, Test.Context(a)"
+          snippet "Instead it is:", type
+          snippet node.expression
+        end
       end
 
       VOID
