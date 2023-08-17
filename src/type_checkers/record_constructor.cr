ca4e039867593f0272dfd5cdff0db96bfe675e07
Szikszai Gusztáv <guszti5@hotmail.com>
2023-08-17 13:03:43 +0200

    Convert record constructor type checking errors.

--- src/type_checkers/record_constructor.cr
+++ src/type_checkers/record_constructor.cr
@@ -1,28 +1,35 @@
 module Mint
   class TypeChecker
-    type_error RecordConstructorArgumentSizeMismatch
-    type_error RecordConstructorArgumentTypeMismatch
-    type_error RecordConstructorNotFoundRecord
-
     def check(node : Ast::EnumId, record : Record) : Checkable
       fields =
         record.fields.values
 
-      raise RecordConstructorArgumentSizeMismatch, {
-        "argument_size" => node.expressions.size.to_s,
-        "size"          => fields.size.to_s,
-        "record"        => record,
-        "node"          => node,
-      } if node.expressions.size > fields.size
+      error :record_constructor_argument_size_mismatch do
+        block do
+          text "The record you tried to create has"
+          bold fields.size.to_s
+          text "fields, while you tried to create it with"
+          bold node.expressions.size.to_s
+        end
+
+        snippet record
+        snippet "You tried to create it here:", node
+      end if node.expressions.size > fields.size
 
       node.expressions.each_with_index do |argument, index|
         type = resolve argument
 
-        raise RecordConstructorArgumentTypeMismatch, {
-          "expected" => fields[index],
-          "node"     => argument,
-          "got"      => type,
-        } unless Comparer.compare(type, fields[index])
+        error :record_constructor_argument_type_mismatch do
+          block do
+            text "The supplied values type does not match the type"
+            text "of the field."
+          end
+
+          snippet "The type of the field is:", fields[index]
+          snippet "You tried to supply it with:", type
+
+          snippet argument
+        end unless Comparer.compare(type, fields[index])
       end
 
       types[node] = record
