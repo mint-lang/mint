82da8dd2a53493cfd1ed454b20f864ae2f24418d
Szikszai Gusztáv <guszti5@hotmail.com>
2023-08-17 06:50:49 +0200

    Convert next call type checking errors.

--- src/type_checkers/next_call.cr
+++ src/type_checkers/next_call.cr
@@ -1,15 +1,17 @@
 module Mint
   class TypeChecker
-    type_error NextCallInvalidInvocation
-    type_error NextCallStateTypeMismatch
-    type_error NextCallStateNotFound
-
     def check(node : Ast::NextCall) : Checkable
       entity = stateful?
 
-      raise NextCallInvalidInvocation, {
-        "node" => node,
-      } unless entity
+      error :next_call_invalid_invocation do
+        block do
+          text "A"
+          bold "next call"
+          text "can only called inside a component or store."
+        end
+
+        snippet node
+      end unless entity
 
       node.data.fields.each do |item|
         name =
@@ -33,10 +35,15 @@ module Mint
               .find(&.name.value.==(name))
           end
 
-        raise NextCallStateNotFound, {
-          "name" => name,
-          "node" => node,
-        } unless state
+        error :next_call_state_not_found do
+          block do
+            text "I was looking for a state named"
+            bold name
+            text "but could not find it."
+          end
+
+          snippet node
+        end unless state
 
         type =
           resolve item.value
@@ -44,13 +51,13 @@ module Mint
         state_type =
           resolve state
 
-        raise NextCallStateTypeMismatch, {
-          "name"     => name,
-          "expected" => state_type,
-          "state"    => state,
-          "node"     => item,
-          "got"      => type,
-        } unless Comparer.compare(state_type, type)
+        error :next_call_state_type_mismatch do
+          block "You were trying to assign an incompatible value to the status state."
+          snippet "The type of the state is:", state_type
+          snippet "But the type you are trying to assign to it:", type
+          snippet "Here is where you did the assignment:", item
+          snippet "And here is where the state is defined:", state
+        end unless Comparer.compare(state_type, type)
       end
 
       Type.new("Promise", [VOID] of Checkable)
