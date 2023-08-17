d9fa6d038d34e16fa42736436811abdfa232e12c
Szikszai Gusztáv <guszti5@hotmail.com>
2023-08-17 13:48:35 +0200

    Convert route type checking errors.

--- src/type_checkers/route.cr
+++ src/type_checkers/route.cr
@@ -1,17 +1,21 @@
 module Mint
   class TypeChecker
-    type_error RouteParamInvalid
-
     def check(node : Ast::Route) : Checkable
       args = node.arguments.map do |argument|
         argument_type =
           resolve argument
 
-        raise RouteParamInvalid, {
-          "name" => argument.name.value,
-          "got"  => argument_type,
-          "node" => argument,
-        } unless Comparer.matches_any?(argument_type, [STRING, NUMBER])
+        error :route_param_invalid do
+          block do
+            text "The type of parameter "
+            bold argument.name.value
+            text "cannot be used in routes."
+          end
+
+          snippet argument_type
+          snippet "Only these types can be used as route params:", "String, Number"
+          snippet argument
+        end unless Comparer.matches_any?(argument_type, [STRING, NUMBER])
 
         {argument.name.value, argument_type, argument}
       end
