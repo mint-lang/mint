a33506e5edde0211d7f02ef9546816749ec0f116
Szikszai Gusztáv <guszti5@hotmail.com>
2023-09-23 17:24:53 +0200

    Implement @highlight-file directive.

---
+++ spec/fixtures/Test.mint
@@ -0,0 +1,5 @@
+component Main {
+  fun render : Html {
+    <div></div>
+  }
+}
---
+++ src/parsers/directives/highlight_file.cr
@@ -0,0 +1,33 @@
+module Mint
+  class Parser
+    def highlight_file_directive : Ast::Directives::HighlightFile?
+      parse do |start_position|
+        next unless word! "@highlight-file"
+        whitespace
+
+        next error :highlight_file_directive_expected_opening_parenthesis do
+          expected "the opening parenthesis of an highlight file directive", word
+          snippet self
+        end unless char! '('
+        whitespace
+
+        next error :highlight_file_directive_expected_path do
+          expected "the path (to the file) of an highlight file directive", word
+          snippet self
+        end unless path = gather { chars { char != ')' } }.presence.try(&.strip)
+        whitespace
+
+        next error :highlight_file_directive_expected_closing_parenthesis do
+          expected "the closing parenthesis of an highlight file directive", word
+          snippet self
+        end unless char! ')'
+
+        Ast::Directives::HighlightFile.new(
+          from: start_position,
+          to: position,
+          file: file,
+          path: path)
+      end
+    end
+  end
+end
