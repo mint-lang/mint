606d9576114745bd53932c12095c7634dc42984a
Szikszai Gusztáv <guszti5@hotmail.com>
2025-03-31 06:54:13 +0200

    Resolve type definitions of record fields properly.

--- src/type_checkers/type_definition_field.cr
+++ src/type_checkers/type_definition_field.cr
@@ -1,7 +1,11 @@
 module Mint
   class TypeChecker
     def check(node : Ast::TypeDefinitionField) : Checkable
-      resolve node.type
+      resolve(node.type).tap do |type|
+        if definition = ast.type_definitions.find(&.name.value.==(type.name))
+          resolve(definition)
+        end
+      end
     end
   end
 end
